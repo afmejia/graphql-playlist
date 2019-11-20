cca4f646a2b4099c44860af48d6dcf14e5bdd116
afmejia <afmejia23@utp.edu.co>
2019-11-20 06:06:36 -0500

    Creating new schema for authors

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
-    
-}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
 
-app.listen('4000', () => {
-    console.log('now listening on port 4000');
+app.listen("4000", () => {
+  console.log("now listening on port 4000");
 });
